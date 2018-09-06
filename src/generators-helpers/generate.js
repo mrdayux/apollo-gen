863923bb607e23d6396df800b47f664e69e83511
Dario Ronzoni <dario.ronzoni@f2.net>
2018-09-06 16:46:02 +0200

    add generate directive

---
+++ src/generators-helpers/generate.js
@@ -0,0 +1,22 @@
+function generate(entry, methods, language, options) {
+    if (!entry.directives || !entry.directives.generate) {
+        return options.fn(this);
+    } else {
+        const methodsArray = methods.split(',');
+        let show = methodsArray.reduce((acc, method) => {
+            return acc || !!entry.directives.generate[method];
+        }, false);
+
+        if (show) {
+            return options.fn(this);
+        }
+    }
+
+    if (language === 'gql') {
+        return `## Not generated - ${options.fn(this)}`;
+    } else {
+        return `// ${options.fn(this).replace(/\n/gi, '\n// ')}`;
+    }
+}
+
+module.exports = generate;
